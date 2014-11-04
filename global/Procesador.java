7f8d811f568550451872a8bf913fefc8631a6719
Jorge González Valencia <jorgegvalencia@gmail.com>
2014-11-04 22:57:56 +0100

    Procesador.java changes

--- global/Procesador.java
+++ global/Procesador.java
@@ -1,14 +1,34 @@
 package global;
 
+import global.tabla.TablaSimbolos;
+
+import java.util.Stack;
+
 import lexico.AnalizadorLexico;
 
 public class Procesador {
 
 	public static void main(String[] args) {
 		String fichero = args[1];
-                Tablas tabla = new Tablas();
-		AnalizadorLexico anLex = new AnalizadorLexico(fichero,tabla);
+		TablaSimbolos TSGlobal = new TablaSimbolos();
+		Stack<TablaSimbolos> pilaTS = new Stack<TablaSimbolos>();
+		// Crear tabla de símbolos global en la pila
+		pilaTS.push(new TablaSimbolos());
+		AnalizadorLexico anLex = new AnalizadorLexico(fichero/*,pilaTS*/);
 		//AnalizadorSintactico anSin = new AnalizadorSintactico();
+		/*
+		while(!(anLex.getCaracter().equals("$"))){
+
+			 anSin.pedirToken(anLex);
+			 
+			 // anSin calcula el siguiente estado, actualizando sus variables
+			 // y aplicando las anotaciones semánticas
+			 
+			 anLex.setTablaActual(anSin.getTablaActual());			 
+			 
+			
+		}
+		*/
 	}
 
 }
