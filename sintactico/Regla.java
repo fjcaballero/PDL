2e33041b1728fae329ec6b39053fd45d151a4eed
Javichuck <javichuck@hotmail.es>
2014-12-29 22:32:25 +0100

    Método analizar + clase Regla

---
+++ sintactico/Regla.java
@@ -0,0 +1,15 @@
+package sintactico;
+
+public class Regla {
+	
+	public int numero;
+	public String parteIzq;
+	public int nElementosDer;
+	
+	public Regla(int numero, String parteIzq, int nElementosDer){
+		this.numero = numero;
+		this.parteIzq = parteIzq;
+		this.nElementosDer = nElementosDer;
+	}
+
+}
